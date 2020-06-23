946009d058ed4d5e37e3b5616704028f12a9e648
Ubuntu <ubuntu@ip-172-31-24-96.us-east-2.compute.internal>
2020-06-23 18:00:01 +0000

    testing sinatra

--- tom_calendar.rb
+++ tom_calendar.rb
@@ -1,10 +1,25 @@
 #! /usr/bin/env ruby
 
 require 'sinatra'
+require 'json'
+
 
 set :bind, '0.0.0.0'
 set :port, 8000
 
+enable :sessions
+
+
+get '/foo' do
+  request.env.to_json
+end
+
+get '/google' do
+  logger.info 'about to redirect to google'
+  redirect to('https://google.com')
+end
+
 get '/' do
-  'Tom Calendar testttt'
+  session[:testing] = 'session test'
+  erb :index, :locals => {test_var: session[:testing]}
 end
