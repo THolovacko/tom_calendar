8b68d3e01dd9cecbb972a2620a8276cb1ae45461
tholovacko <tom.holovacko@gmail.com>
2020-07-08 16:03:11 +0000

    almost almost finsihed with authorization

--- lib/tom_calendar.rb
+++ lib/tom_calendar.rb
@@ -26,7 +26,8 @@ end
 def get_google_authorizer(dynamodb_connection)
   client_id   = Google::Auth::ClientId.new(ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'])
   token_store = DynamoDBTokenStore.new(dynamodb_connection)
-  authorizer  = Google::Auth::UserAuthorizer.new(client_id, GOOGLE_PERMISSION_SCOPES, token_store)
+  #authorizer  = Google::Auth::UserAuthorizer.new(client_id, GOOGLE_PERMISSION_SCOPES, token_store, 'https://tomcalendar.com/authorize_gmail')
+  authorizer  = Google::Auth::UserAuthorizer.new(client_id, GOOGLE_PERMISSION_SCOPES, token_store, 'postmessage')
   authorizer
 end
 
