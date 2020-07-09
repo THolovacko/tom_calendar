e565c5202ddda440c32c8bc99e23077b6918b182
tholovacko <tom.holovacko@gmail.com>
2020-07-09 15:34:14 +0000

    almost finsihed login

--- lib/tom_calendar.rb
+++ lib/tom_calendar.rb
@@ -2,6 +2,8 @@ require 'aws-sdk-dynamodb'
 require 'googleauth'
 require 'googleauth/web_user_authorizer'
 require 'googleauth/token_store'
+require 'jwt'
+require 'json'
 
 GOOGLE_PERMISSION_SCOPES = ['profile', 'email', 'https://www.googleapis.com/auth/calendar'].freeze
 
@@ -10,16 +12,48 @@ class DynamoDBTokenStore < Google::Auth::TokenStore
     @dynamodb = dynamodb_connection
   end
 
-  def self.default()
-  end
-
   def store(id, token)
+    item = JSON.parse(token)
+    item[:username] = id
+    
+    params = {
+      table_name: 'GoogleTokens',
+      item: item
+    }
+
+    begin
+      dynamodb.put_item(params)
+    rescue  Aws::DynamoDB::Errors::ServiceError => error
+      raise "Unable to add token: #{item.to_s}"
+    end
   end
 
   def load(id)
+    params = {
+      table_name: 'GoogleTokens',
+      key: { username: id }
+    }
+
+    begin
+      item = dynamodb.get_item(params).item
+    rescue  Aws::DynamoDB::Errors::ServiceError => error
+      raise "Unable to get token for id: #{id}"
+    end
+
+    return item.to_json
   end
 
   def delete(id)
+    params = {
+      table_name: 'GoogleTokens',
+      key: { username: id }
+    }
+
+    begin
+      dynamodb.delete_item(params)
+    rescue  Aws::DynamoDB::Errors::ServiceError => error
+      raise "Unable to delete token for id: #{id}"
+    end
   end
 end
 
@@ -28,6 +62,14 @@ def get_google_authorizer(dynamodb_connection)
   token_store = DynamoDBTokenStore.new(dynamodb_connection)
   authorizer  = Google::Auth::UserAuthorizer.new(client_id, GOOGLE_PERMISSION_SCOPES, token_store, 'postmessage')
   authorizer
+end
+
+def decode_google_id_token(id_token)
+  user_data = JWT.decode(id_token, nil, false).first
+  raise 'invalid iss' unless user_data['iss'] == 'https://accounts.google.com'.freeze
+  raise 'invalid aud' unless user_data['aud'] == ENV['GOOGLE_OAUTH_CLIENT_ID']
+  raise 'invalid exp' unless user_data['exp'] > 0
+  user_data
 end
 
 
