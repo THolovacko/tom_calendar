6201623a10875d48df0125868c57336a9937113a
tomh <tom.holovacko@gmail.com>
2021-01-13 10:22:38 -0500

    remote deploy Wed Jan 13 10:22:38 EST 2021

--- quick_remote_deploy.sh
+++ quick_remote_deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
 
-scp -r ../tom_calendar/ ubuntu@tomcalendar.com:/home/ubuntu/
+rsync -avzhe ssh ../tom_calendar/ ubuntu@tomcalendar.com:/home/ubuntu/
 
 ssh ubuntu@tomcalendar.com "deploy_tom_calendar.sh"
