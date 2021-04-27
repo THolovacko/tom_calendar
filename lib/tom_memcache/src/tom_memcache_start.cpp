2be97f025d524647190b34b40ab85da8402aca58
tholovacko <tom.holovacko@gmail.com>
2021-04-27 17:15:26 +0000

    implementing custom hash map cache thing

--- lib/tom_memcache/src/tom_memcache_start.cpp
+++ lib/tom_memcache/src/tom_memcache_start.cpp
@@ -3,13 +3,62 @@
 #include <queue>
 #include <condition_variable>
 #include <chrono>
-#include <unordered_map>
+#include <atomic>
+#include <cstdlib>
+#include <functional>
+#include <memory>
 #include "tom_memcache.h"
 
 // @remember: do I need to increase OS default socket buffer size?
 // @remember: clients will need to verify recieved message isn't coming from late result for other client
 // @remember: need to review bucket reserve count and memory percentage allocated for cache
 // @current: implemement custom concurrent hash map cache thing
+
+
+struct tom_timed_map {
+  struct entry {
+    std::string key;
+    std::string value;
+    std::chrono::time_point<std::chrono::system_clock> expiration_time;
+
+    entry() { expiration_time = std::chrono::system_clock::now(); }
+    entry(const std::string& p_key, const std::string& p_value, std::size_t expiration_delay_seconds) : key(p_key), value(p_value), expiration_time(std::chrono::system_clock::now() + std::chrono::seconds(expiration_delay_seconds) ) {}
+  };
+
+  typedef std::shared_ptr<entry> bucket;
+
+
+  const std::size_t bucket_count;
+  bucket* const buckets;
+  
+  tom_timed_map(const std::size_t p_bucket_count) : bucket_count(p_bucket_count), buckets(new bucket[p_bucket_count]) {}
+  
+  void set(const std::string& set_key, const std::string& set_value, const std::size_t expiration_seconds) {
+    size_t bucket_index = std::hash<std::string>{}(set_key) % bucket_count;
+    std::atomic_store( &(buckets[bucket_index]), std::make_shared<entry>(set_key,set_value,expiration_seconds) );
+  }
+
+  const std::string search(const std::string& search_key) const {
+    size_t bucket_index = std::hash<std::string>{}(search_key) % bucket_count;
+
+    std::shared_ptr<entry> search_result;
+    search_result = std::atomic_load( &(buckets[bucket_index]) );
+
+    if ( (search_result->expiration_time <= std::chrono::system_clock::now()) ) {
+      return "";
+    } else {
+      if (search_result->key.compare(search_key) != 0) {
+        return "";
+      } else {
+        return search_result->value;
+      }
+    }
+  }
+
+  ~tom_timed_map() {
+   delete[] buckets;
+  }
+};
 
 tom_socket server_socket(SERVER_IP_ADDRESS, SERVER_PORT, true);
 bool is_server_running = true;
@@ -18,7 +67,7 @@ std::vector<std::queue<tom_socket::client_message>*> thread_queues;
 std::vector<std::mutex*> thread_mutexes;
 std::vector<std::condition_variable*> thread_condition_variables;
 std::vector<bool> thread_flags; // @remember: make atomic variable?
-std::unordered_map<std::string, std::string>* tom_cache;
+tom_timed_map* tom_cache;
 const long int max_ram_bytes = sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGESIZE);
 const long int max_cache_ram_bytes = max_ram_bytes / 3;
 
@@ -29,15 +78,15 @@ void worker_thread(const std::size_t pool_index) {
 
       switch(current_client_data.message[0]) {  // decide if get or set
         case 'i'  :
-          server_socket.respond_to_client( "bucket count: " + std::to_string(tom_cache->bucket_count()) + "\n" + "buckets used: " + std::to_string(tom_cache->size()), current_client_data.address, current_client_data.address_length);
+          server_socket.respond_to_client( "bucket count: " + std::to_string(tom_cache->bucket_count), current_client_data.address, current_client_data.address_length);
           break;
         case 'g'  :
           // @current: check timestamp and return empty string if expired
-          server_socket.respond_to_client( (*tom_cache)[ current_client_data.message.substr(4) ], current_client_data.address, current_client_data.address_length);  // 4 is the length of "get "
+          server_socket.respond_to_client( tom_cache->search(current_client_data.message.substr(4)), current_client_data.address, current_client_data.address_length);  // 4 is the length of "get "
           break;
         case 's'  :
           std::size_t delimiter_position = current_client_data.message.find("%*=tom-cache-delim=*08071992%");
-          (*tom_cache)[current_client_data.message.substr(4, delimiter_position - 4)] = current_client_data.message.substr(delimiter_position + 29); // 4 is length of "set " and 29 is length of delimiter
+          tom_cache->set(current_client_data.message.substr(4, delimiter_position - 4), current_client_data.message.substr(delimiter_position + 29), 10); // 4 is length of "set " and 29 is length of delimiter
 
           // @current: manually track memory with atomic long int (std::atomic)
           // @current: check memory threshold and remove stuff
@@ -58,6 +107,7 @@ void worker_thread(const std::size_t pool_index) {
 }
 
 
+
 int main() {
   // decide thread count
   const std::size_t hardware_thread_count = std::thread::hardware_concurrency();
@@ -73,8 +123,7 @@ int main() {
   const long int bucket_count = (max_ram_bytes / ram_divisor) / assumed_bucket_size_bytes;
   */
 
-  tom_cache = new std::unordered_map<std::string, std::string>();
-  tom_cache->reserve(100000);
+  tom_cache = new tom_timed_map(100000);
 
   // initalize thread pool and thread queues 
   for (std::size_t i=0; i < thread_count; ++i) {
