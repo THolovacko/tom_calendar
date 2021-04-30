2ba7e96b50bc3fd2c8bb2fcd5a081334302955e5
tholovacko <tom.holovacko@gmail.com>
2021-04-30 13:25:40 +0000

    minor change

--- lib/tom_memcache/restart.sh
+++ lib/tom_memcache/restart.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
 /home/ubuntu/tom_calendar/lib/tom_memcache/build.sh
-tom_memcache_stop
+tom_memcache_stop > /dev/null
 tom_memcache_start &
+tput setaf 2
+  echo "restarted tom_memcache"
+tput setaf 0
