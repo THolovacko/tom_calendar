01b28e1f2133823221982e0df1db2b64b36d4af5
tholovacko <tom.holovacko@gmail.com>
2021-05-01 15:31:51 +0000

    thinking about using jumbograms to cache dashboard

--- lib/tom_memcache/src/tom_memcache_get.cpp
+++ lib/tom_memcache/src/tom_memcache_get.cpp
@@ -17,15 +17,13 @@ int main(int argc, char* argv[]) {
   if ( server_response.length() > (key.length() + 1) ) {  // length must be greater than length of "key:" for it to not be empty string
     server_key = std::string(server_response.substr(0, key.length()));
   } else {
-    printf("\n");
     return 0;
   }
 
   if ( server_key.compare(key) == 0 ) { // server returns "key:value" (because added ':' we don't length - 1)
-    printf("%s\n", server_response.substr(key.length() + 1).data());
+    printf("%s", server_response.substr(key.length() + 1).data());
     return 0;
   } else {
-    printf("\n");
     return 0;
   }
 }
