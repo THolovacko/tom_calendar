9d242a85c3ca74a1245fee3799ba08f59b536df3
tholovacko <tom.holovacko@gmail.com>
2021-05-07 13:01:25 +0000

    fixed cache bug

--- lib/tom_memcache.rb
+++ lib/tom_memcache.rb
@@ -1,11 +1,11 @@
 module TomMemcache
   def self.get(key)
-    result = `tom_memcache_get "#{key}"`.freeze
+    result = `tom_memcache_get '#{key}'`.freeze
     result = nil if (result == '')
     result
   end
 
   def self.set(key, value, expiration_in_seconds)
-    `tom_memcache_set "#{key}" "#{value}" "#{expiration_in_seconds}"`
+    `tom_memcache_set '#{key}' '#{value}' '#{expiration_in_seconds}'`
   end
 end
