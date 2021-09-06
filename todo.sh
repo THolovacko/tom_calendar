67308472f0733e779e6e96061fda08cc5398ab11
tholovacko <tom.holovacko@gmail.com>
2021-09-06 17:35:29 +0000

    fixed user profile bug

--- todo.sh
+++ todo.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-grep -Er '@remember|@optimize|@test|@refactor' --color -n --exclude-dir=html --exclude=todo.sh
+grep -Er '@remember|@optimize|@test|@refactor|@hack' --color -n --exclude-dir=html --exclude=todo.sh
 grep -Er '@current' --color -n --exclude-dir=html --exclude=todo.sh
